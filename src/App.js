a6af32e4d6d808e9b92c308f99738ea80da1730d
Jithin Balan <45451221+jithinbalan@users.noreply.github.com>
2023-10-08 13:55:40 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import {React,useState} from 'react';
 import './App.css';
+import Scanner from './components/Scanner';
 
 function App() {
+    const [camera, setCamera] = useState(false);
+  const [result, setResult] = useState(null);
+
+  const onDetected = result => {
+    setResult(result);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{result ? result : "Scanning..."}</p>
+      <button onClick={() => setCamera(!camera)}>
+        {camera ? "Stop" : "Start"}
+      </button>
+      <div className="container">
+        {camera && <Scanner onDetected={onDetected} />}
+      </div>
     </div>
   );
 }
